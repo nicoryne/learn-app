b4808bf30271222a31d3b087b893b6e5af8db416
Antonio Mancera Ubaldo Jr <142708326+JunSayke@users.noreply.github.com>
2024-03-04 23:21:13 +0800

    [Hunyo] Refactoring.

--- SolutionsProject/app/src/main/java/com/example/solutionsproject/activities/MainActivity.java
+++ SolutionsProject/app/src/main/java/com/example/solutionsproject/activities/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flashcard);
+        setContentView(R.layout.activity_main);
 
 //        SessionManager session = new SessionManager(getApplicationContext());
 //        if (session.isLoggedIn()) {
